189e830a0c7667eb185e8f491ff196a2a05b61b6
mike.li2 <mike.li2@workday.com>
2018-01-13 17:01:33 -0800

    added nginx tests server

--- fake_api_server/server2.js
+++ fake_api_server/server2.js
@@ -8,24 +8,24 @@ var router = jsonServer.router('urls.json')
 var middlewares = jsonServer.defaults()
 
 var count = 0;
-
 server.use(middlewares)
 
 server.use(jsonServer.bodyParser)
+
 server.use(function (req, res, next) {
     if (req.method === 'POST') {
         // Converts POST to GET and move payload to query params
         // This way it will make JSON Server that it's GET request
         req.method = 'GET'
         req.query = req.body
+        count += 1;
+        console.log("I am handling request! " + 3001 + " times: " + count);
     }
-    count += 1;
-    console.log("I am handling request! " + 3000 + " times: " + count);
     // Continue to JSON Server router
     next()
 })
 
 server.use(router)
-server.listen(3000, function () {
+server.listen(3001, function () {
     console.log('JSON Server is running')
 })
